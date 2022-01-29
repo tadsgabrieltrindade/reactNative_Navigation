d9b6964b38d6ec745d5d4cd3b2946baa3a9b2d7c
Gabriel Trindade <icandoitgrt@gmail.com>
2022-01-29 14:15:51 -0300

    Stack Navigation

---
+++ src/pages/Information/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Information() {
+  return (
+    <View >
+      <Text>Nome: João Botelho</Text>
+      <Text>Telefone: (11) 93300-8587</Text>
+      <Text>Endereço: Rua das araucarias</Text>
+      <Text>N°: 1759</Text>
+      <Text>Profissão: Carpinteiro</Text>
+      <Text>Email: joaobotelho@carpintariabotelho.com.br</Text>
+    </View>
+  );
+}
